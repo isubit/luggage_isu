d09b86053700f39f80c683d1ede0e918cae530cd
michael hofmockel <michael.hofmockel@gmail.com>
2015-01-28 14:09:25 -0600

    pubcookie to Shibboleth script

---
+++ scripts/pubcookie_to_shibboleth.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Assumptions:
+# ===========
+# - Drush 5.9 or greater
+# - Git 1.8.5.2 or greater
+# - Execution from a shell in the webroot directory of luggage_isu.
+#
+# Example:
+#
+# cd luggage_isu (cd %your_site)
+# ./scripts/pubcookie_to_shibboleth.sh
+#
+
+rm -rf sites/all/modules/luggage/luggage_pubcookie
+git rm -rf sites/all/modules/pubcookie
+rm -rf sites/all/modules/pubcookie
+drush en isushib isushibsiteaccess -y
